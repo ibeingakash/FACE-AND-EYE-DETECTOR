5986713a7883d52bd05c5af0f41634d7c8ddda66
ibeingakash <75716531+ibeingakash@users.noreply.github.com>
2021-08-19 19:41:56 +0530

    Update eye_detector.py

--- eye_detector.py
+++ eye_detector.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
+#importing the libraries
 
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 
 
 cap = cv2.VideoCapture(0)
